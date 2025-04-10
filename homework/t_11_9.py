458905bf9cf8c065417139201f4efe09d4d10d1b
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-10 22:58:49 +0300

    added 11_9

---
+++ homework/t_11_9.py
@@ -0,0 +1,19 @@
+def f_a(n):
+    P_n = 3/4
+    for i in range(3, n+1):
+        P_n *= (1 - 1 / (n**2))
+    return P_n
+
+def f_b(n):
+    a_n = 1
+    P_n = 1
+    for i in range(2, n+1):
+        a_n *= n
+        P_n *= 2 + 1/a_n
+    return P_n
+
+def f_c(n):
+    #return 2/(n+2)
+    P_n = 2/3
+    for i in range(2, n+1):
+        P_n *= (n+1)/(n+2)
