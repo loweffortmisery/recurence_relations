f17cbbdb94675268edb47125d7f384f0d9986f3e
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-12 18:36:35 +0300

    mistake fix

--- homework/t_11_7.py
+++ homework/t_11_7.py
@@ -1,15 +1,15 @@
-def f_a(x, k):
+def f_a(x, k = -1):
     x_k = x
+    yield x_k
     for i in range(1,k+1):
-        x_k *= (x^2)/((2*k)*(2*k+1))
-    return x_k
-
+        x_k *= (x^2)/((2*i)*(2*i+1))
+        yield x_k
 def f_b(x, k):
     a_k = -x
     x_k = a_k
     for i in range(2,k+1):
         a_k = -x * a_k
-        x_k = a_k / k
+        x_k = a_k / i
     return x_k
 
 def f_c(x, k):
@@ -18,7 +18,7 @@ def f_c(x, k):
     x_k = a_k / b_k
     for i in range(1, k+1):
         a_k = -x * a_k
-        for j in range(k**2 - k + 1, k**2 + k + 1):
+        for j in range(i**2 - i + 1, i**2 + i + 1):
             b_k *= j
         x_k = a_k / b_k
     return x_k
@@ -30,7 +30,7 @@ def f_d(x, k):
     for i in range(1, k+1):
         a_k = x * a_k
         b_k = k * b_k
-        x_k = (k+1) * a_k / b_k
+        x_k = (i+1) * a_k / b_k
     return x_k
 
 
