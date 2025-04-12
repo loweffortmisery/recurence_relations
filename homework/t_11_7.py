85cf7d2b50b32ac7d405a32a5c24aa5c02bd8bf8
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-12 18:41:33 +0300

    recoded functions to functions-generators

--- homework/t_11_7.py
+++ homework/t_11_7.py
@@ -1,37 +1,50 @@
 def f_a(x, k = -1):
     x_k = x
-    yield x_k
-    for i in range(1,k+1):
+    i = 0
+    yield x_k, i
+    while (i != k):
+        i += 1
         x_k *= (x^2)/((2*i)*(2*i+1))
-        yield x_k
-def f_b(x, k):
+        yield x_k, i
+
+def f_b(x, k = -1):
     a_k = -x
     x_k = a_k
-    for i in range(2,k+1):
+    i = 1
+    yield x_k, i
+    while i != k:
+        i += 1
         a_k = -x * a_k
         x_k = a_k / i
-    return x_k
+        yield x_k, i
 
-def f_c(x, k):
+def f_c(x, k = -1):
     a_k = 1
     b_k = 1
     x_k = a_k / b_k
-    for i in range(1, k+1):
+    i = 0
+    yield x_k, i
+    while i != k:
+        i += 1
         a_k = -x * a_k
         for j in range(i**2 - i + 1, i**2 + i + 1):
             b_k *= j
         x_k = a_k / b_k
-    return x_k
+        yield x_l, i
 
-def f_d(x, k):
+def f_d(x, k = -1):
     a_k = 1
     b_k = 1
     x_k = a_k/b_k
-    for i in range(1, k+1):
+    i = 0
+    yield x_k, i
+    while i != k:
+        i += 1
         a_k = x * a_k
         b_k = k * b_k
         x_k = (i+1) * a_k / b_k
-    return x_k
+        yield x_k, i
+ 
 
 
 
