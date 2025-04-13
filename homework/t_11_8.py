d7a6c59a4de35f7905af743fc6d1101b42806c71
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-13 09:16:23 +0300

    recoded functions to generators

--- homework/t_11_8.py
+++ homework/t_11_8.py
@@ -1,24 +1,33 @@
 def f_a(n):
     a_n = 1
     S_n = 1
-    for i in range(2, n+1):
+    i = 1
+    yield S_n, i
+    while(i != n):
+        i += 1
         a_n = -a_n
         S_n += a_n * i
-    return S_n
+        yield S_n, i
 
 def f_b(n):
     # return 1 - 1/n
     S_n = 1/2
-    for i in range(3, n+1):
+    i = 2
+    yield S_n, i
+    while(i != n):
+        i += 1
         S_n += 1/((i-1)*i)
-    return S_n
+        yield S_n, i
 
 def f_c(n):
     a_n = 1
     S_n = 1/2
-    for i in range(3, n+1):
+    i = 2
+    yield S_n, i
+    while(i != n):
+        i += 1
         a_n = -a_n
         S_n += a_n * (i-1)/i
-    return S_n
+        yield S_n, i
 
 
