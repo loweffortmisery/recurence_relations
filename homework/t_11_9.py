338ca19fab48b4554a363141fb6c80c73eac707b
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-13 16:03:01 +0300

    recoded functions to generators

--- homework/t_11_9.py
+++ homework/t_11_9.py
@@ -1,19 +1,29 @@
-def f_a(n):
+def f_a(n = -1):
     P_n = 3/4
-    for i in range(3, n+1):
+    i = 2
+    yield P_n, i
+    while i != n:
+        i += 1
         P_n *= (1 - 1 / (i**2))
-    return P_n
 
-def f_b(n):
+def f_b(n = -1):
     a_n = 1
     P_n = 1
-    for i in range(2, n+1):
+    i = 1
+    yield P_n, i
+    while i != n:
+        i += 1
         a_n *= i
         P_n *= 2 + 1/a_n
-    return P_n
+        yield P_n, i
 
-def f_c(n):
+def f_c(n = -1):
     #return 2/(n+2)
     P_n = 2/3
-    for i in range(2, n+1):
+    i = 1
+    yield P_n, i
+    while i != n:
+        i += 1
         P_n *= (i+1)/(i+2)
+        yield P_n, i
+
