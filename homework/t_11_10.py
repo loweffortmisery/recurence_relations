7371e71d23eb00294f9d599ecdc4cf4ef95af0c8
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-04-13 16:13:51 +0300

    recoded functions to generators

--- homework/t_11_10.py
+++ homework/t_11_10.py
@@ -1,8 +1,11 @@
-def f(n):
-    b_n = 4*n + 2
-    y_n = b_n
-    for i in range(2, n):
-        b_n -= 4
-        y_n = b_n + 1/y_n
-    F_n = 2 + 1/y_n
-    return F_n
+def f(n = -1):
+    i = 0
+    while i != n:
+        i += 1
+        b_n = 4*i + 2
+        y_n = b_n
+        for j in range(2, i+1)
+            b_n -= 4
+            y_n = b_n + 1/y_n
+        F_n = 2 + 1/y_n
+        yield F_n, i
